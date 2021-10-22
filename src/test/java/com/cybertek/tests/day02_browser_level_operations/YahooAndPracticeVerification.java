32311e72ae0b74cf4531553c52894c0210832572
Murodil <murodil@cybertekschool.com>
2021-10-21 22:05:59 -0400

    - TC1 adn TC2 code

---
+++ src/test/java/com/cybertek/tests/day02_browser_level_operations/YahooAndPracticeVerification.java
@@ -0,0 +1,32 @@
+package com.cybertek.tests.day02_browser_level_operations;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class YahooAndPracticeVerification {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.yahoo.com");
+
+        String expectedTitle = "Yahoo | Mail, Weather, Search, Politics, News, Finance, Sports & Videos";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)) {
+            System.out.println("PASS: title verification successful");
+        } else {
+            System.out.println("FAIL: title verification failed");
+        }
+        System.out.println("actualTitle = " + actualTitle);
+        System.out.println("expectedTitle = " + expectedTitle);
+
+//        if(driver.getTitle().equals("Yahoo | Mail, Weather, Search, Politics, News, Finance, Sports & Videos")) {
+//
+//        }
+
+
+        driver.quit();
+
+    }
+}
