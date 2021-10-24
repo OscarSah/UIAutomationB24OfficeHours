ca7d56b4865aac9b292d681fee1ce4abe293c484
Murodil <murodil@cybertekschool.com>
2021-10-24 15:42:07 -0400

    - TC5 complete code

--- src/test/java/com/cybertek/tests/day03_locators_intro/TC5_SmartBearLoginTest.java
+++ src/test/java/com/cybertek/tests/day03_locators_intro/TC5_SmartBearLoginTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC5_SmartBearLoginTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String appUrl = "http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx";
         String userName = "Tester";
         String password = "test";
@@ -21,12 +21,36 @@ public class TC5_SmartBearLoginTest {
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys(userName);
         //enter password
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys(password);
+
+        Thread.sleep(2000);
         //click login
         driver.findElement(By.id("ctl00_MainContent_login_button")).click();
 
         //verify title is "Web Orders"
+        Thread.sleep(2000);
+        String expTitle = "Web Orders"; //required title from documentation
+        String actTitle = driver.getTitle(); //actual coming from selenium browser
+
+        if (actTitle.equals(expTitle)) { //compare if both match or no
+            System.out.println("PASS: login success. title match");
+        } else {
+            System.out.println("FAIL: login failed. title mismatch");
+        }
+        //check title without variables
+        if(driver.getTitle().equals("Web Orders")) {
+            System.out.println("PASS: login success. title match");
+        } else {
+            System.out.println("FAIL: login failed. title mismatch");
+        }
+
+        //click logout link
+        driver.findElement(By.linkText("Logout")).click();
+
+        Thread.sleep(2000);
+
+        //quit browser
+        driver.quit();
 
 
-        driver.close();
     }
 }
