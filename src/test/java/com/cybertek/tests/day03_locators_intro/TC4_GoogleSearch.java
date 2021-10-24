26d1bfa6d92b39263c3717656c5721eb3b5b3eb2
Murodil <murodil@cybertekschool.com>
2021-10-24 12:55:19 -0400

    - TC4 code

--- src/test/java/com/cybertek/tests/day03_locators_intro/TC4_GoogleSearch.java
+++ src/test/java/com/cybertek/tests/day03_locators_intro/TC4_GoogleSearch.java
@@ -1,17 +1,41 @@
 package com.cybertek.tests.day03_locators_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class TC4_GoogleSearch {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         String url = "https://www.google.com";
         driver.get(url);
+        //locate and type "apple" to google search
+        driver.findElement(By.name("q")).sendKeys("apple");
+
+        Thread.sleep(2000);
+        //locate and click on search bottom
+        driver.findElement(By.name("btnK")).click();
+        /**
+         * if above two lines did not work , try this way:
+         * driver.findElement(By.name("q")).sendKeys("apple" + Keys.ENTER);
+         */
+        //driver.findElement(By.name("q")).sendKeys("apple" + Keys.ENTER);
+
+        String actTitle = driver.getTitle();
+        String expTitle = "apple";
+
+        if (actTitle.startsWith(expTitle)) {
+            System.out.println("PASS: Title match");
+        } else {
+            System.out.println("FAIL: Title mismatched");
+        }
+
+        driver.quit();
 
     }
 }
