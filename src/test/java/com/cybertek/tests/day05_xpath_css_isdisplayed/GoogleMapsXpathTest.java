1af12340a3f56e2715c80ead9b94f09ea3d1fbfa
Murodil <murodil@cybertekschool.com>
2021-10-31 14:30:26 -0400

    Absolute xpath using google maps

--- src/test/java/com/cybertek/tests/day05_xpath_css_isdisplayed/GoogleMapsXpathTest.java
+++ src/test/java/com/cybertek/tests/day05_xpath_css_isdisplayed/GoogleMapsXpathTest.java
@@ -7,12 +7,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class GoogleMapsXpathTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         String url = "https://www.google.com/maps";
         driver.get(url);
+        //search for cybertek school
         WebElement addressField = driver.findElement(By.xpath("/html/body/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/form/div/div[3]/div/input[1]"));
         addressField.sendKeys("cybertek school" + Keys.ENTER);
+
+        Thread.sleep(4000);
+
+        //click on directions
+        WebElement directions = driver.findElement(By.xpath("/html/body/div[3]/div[9]/div[8]/div/div[1]/div/div/div[4]/div[1]/button/span/img"));
+        directions.click();
+
+
+
     }
 }
