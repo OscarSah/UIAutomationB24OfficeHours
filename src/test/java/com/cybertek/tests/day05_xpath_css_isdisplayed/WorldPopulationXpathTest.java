3cc2b843cb3a38b74038d5dece37ea7efb3b3efa
Murodil <murodil@cybertekschool.com>
2021-10-31 15:04:52 -0400

    worldpopulation xpath practice

--- src/test/java/com/cybertek/tests/day05_xpath_css_isdisplayed/WorldPopulationXpathTest.java
+++ src/test/java/com/cybertek/tests/day05_xpath_css_isdisplayed/WorldPopulationXpathTest.java
@@ -1,14 +1,21 @@
 package com.cybertek.tests.day05_xpath_css_isdisplayed;
 
 import com.cybertek.utils.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class WorldPopulationXpathTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String appUrl = "https://www.worldometers.info/world-population";
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.get(appUrl);
+
+        Thread.sleep(4000);
+        //<div class="maincounter-number">
+        WebElement worldPopulation = driver.findElement(By.xpath("//div[@class='maincounter-number']"));
+        System.out.println(worldPopulation.getText());
 
     }
 }
