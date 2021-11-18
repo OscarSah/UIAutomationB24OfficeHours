7dabaa768ea3efea834eac2d21bab9eb0f7407a8
OscarSah <oscar@cybertekschool.com>
2021-11-19 02:02:59 +0300

    SmartBear task started

--- src/test/java/com/cybertek/officeHours/oH02/SmartBearTask.java
+++ src/test/java/com/cybertek/officeHours/oH02/SmartBearTask.java
@@ -1,5 +1,12 @@
 package com.cybertek.officeHours.oH02;
 
+import com.cybertek.utils.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
 public class SmartBearTask {
 /*
 Test Case 1:
@@ -21,5 +28,29 @@ After login
  - verify that row of information is deleted
 Hope to see you all and hope to have a productive time together.
  */
+
+    @Test
+    public void loginTest(){
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        // open the page
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        // page Title before login
+        String expectedTitle = "Web Orders Login";
+        String actualTitle = driver.getTitle();
+        System.out.println(expectedTitle.equalsIgnoreCase(actualTitle));
+        // username box info
+        WebElement username = driver.findElement(By.cssSelector("#ctl00_MainContent_username"));
+        username.sendKeys("Tester");
+        // password box
+        WebElement password = driver.findElement(By.cssSelector("#ctl00_MainContent_password"));
+        password.sendKeys("test"+ Keys.ENTER);
+
+
+
+    }
+
 
 }
