b89359b3da3d10a20e2f778ac3f0537afa7b249f
OscarSah <oscar@cybertekschool.com>
2021-11-19 01:48:15 +0300

    Week 2 Notes Added

---
+++ src/test/java/com/cybertek/officeHours/oH02/SmartBearTask.java
@@ -0,0 +1,25 @@
+package com.cybertek.officeHours.oH02;
+
+public class SmartBearTask {
+/*
+Test Case 1:
+Basic login authentication
+- Go to:  http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+- Verify title equals: String expectedTitle = "Web Orders Login"
+- Enter username: Tester
+- Enter password: test
+- Click “Sign In” button
+- Verify title equals: String expectedHomePageTitle = "Web Orders"
+
+Test Case 2:
+After login
+ - click on Check All button
+ - verify that all check buttons are selected
+ - click on Uncheck All button
+ - verify that all buttons are unchecked
+ - select one of the check box and delete that row
+ - verify that row of information is deleted
+Hope to see you all and hope to have a productive time together.
+ */
+
+}
