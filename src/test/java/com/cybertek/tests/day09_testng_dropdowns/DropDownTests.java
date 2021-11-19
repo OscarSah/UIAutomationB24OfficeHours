90db1d2fe2a74015e3b4eeb66aeb988dc063fa7a
Murodil <murodil@cybertekschool.com>
2021-11-18 22:05:27 -0500

    dropdowns intro

--- src/test/java/com/cybertek/tests/day09_testng_dropdowns/DropDownTests.java
+++ src/test/java/com/cybertek/tests/day09_testng_dropdowns/DropDownTests.java
@@ -1,11 +1,12 @@
 package com.cybertek.tests.day09_testng_dropdowns;
 
 import com.cybertek.utils.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,7 +15,7 @@ public class DropDownTests {
     String url = "http://practice.cybertekschool.com/dropdown";
     WebDriver driver;
 
-    @BeforeMethod
+    @BeforeClass
     public void setUp() {
         System.out.println("Setting up WebDriver...");
         driver = WebDriverFactory.getDriver("chrome");
@@ -29,7 +30,17 @@ public class DropDownTests {
         Assert.assertEquals(driver.getTitle(), "Practice");
     }
 
-    @AfterMethod
+    @Test
+    public void simpleDropDownTest() {
+        //locate Select tag element
+        WebElement dropDown = driver.findElement(By.id("dropdown"));
+        //create Select class object
+        Select simpleDropDown = new Select(dropDown);
+        simpleDropDown.selectByVisibleText("Option 1");
+
+    }
+
+    @AfterClass //Runs after each test
     public void tearDown() {
         System.out.println("Closing browser");
         driver.quit();
