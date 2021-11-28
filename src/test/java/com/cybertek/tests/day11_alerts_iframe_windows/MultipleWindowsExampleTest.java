42dd9c6fb32b137c1d64166b336acac0001edcf4
Murodil <murodil@cybertekschool.com>
2021-11-28 16:45:13 -0500

    multiple windows in Selenium

--- src/test/java/com/cybertek/tests/day11_alerts_iframe_windows/MultipleWindowsExampleTest.java
+++ src/test/java/com/cybertek/tests/day11_alerts_iframe_windows/MultipleWindowsExampleTest.java
@@ -14,17 +14,18 @@ import static com.cybertek.utils.WebDriverFactory.getDriver;
 public class MultipleWindowsExampleTest {
     WebDriver driver;
     String url = "http://practice.cybertekschool.com/windows";
+    String demoQaUrl = "https://demoqa.com/browser-windows";
 
     @BeforeMethod
     public void setUp() {
         driver = getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get(url);
     }
 
     @Test
     public void windowsTest() {
+        driver.get(url);
         //get window handle of current window
         String mainWindowHandle = driver.getWindowHandle();
         System.out.println("windowHandle = " + mainWindowHandle);
@@ -35,9 +36,29 @@ public class MultipleWindowsExampleTest {
 
         Set<String> windowsHandles = driver.getWindowHandles();
         System.out.println("Count of windows = " + windowsHandles.size());
+        //print all window handles
+        System.out.println("windowsHandles = " + windowsHandles);
+
+        //loop and switch to a different window
+        for(String windowHandle : windowsHandles) {
+            driver.switchTo().window(windowHandle);
+            System.out.println("Current window title = " + driver.getTitle());
+        }
+
+        WebElement h3Elem = driver.findElement(By.xpath("//h3"));
+        System.out.println("h3 header text on new page = " + h3Elem.getText());
+
+        //go back to parent window/main window
+        driver.switchTo().window(mainWindowHandle);
+        System.out.println("Title after switching back to main window = " + driver.getTitle());
 
     }
 
+    @Test
+    public void demoQAMultipleWindows() {
+        driver.get(demoQaUrl);
+
+    }
 
 }
 
