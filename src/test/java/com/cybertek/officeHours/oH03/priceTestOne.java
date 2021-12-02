20df6a220a79ec06bd27887ae485210a0426c136
OscarSah <oscar@cybertekschool.com>
2021-12-03 01:53:11 +0300

    First Test started

--- src/test/java/com/cybertek/officeHours/oH03/priceTestOne.java
+++ src/test/java/com/cybertek/officeHours/oH03/priceTestOne.java
@@ -4,6 +4,8 @@ import com.cybertek.utils.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -42,9 +44,15 @@ public class priceTestOne {
         driver.findElement(By.cssSelector("#ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.cssSelector("#ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
 
+        // Click  Order button
+        driver.findElement(By.linkText("Order")).click();
 
+        // Verify under Product Information, selected option is “MyMoney”
+        Select dropDown = new Select(driver.findElement(By.cssSelector("#ctl00_MainContent_fmwOrder_ddlProduct")));
+        String expectedText = "MyMone";
+        String actualText = dropDown.getFirstSelectedOption().getText();
 
-
+        Assert.assertEquals(actualText,expectedText,"Text do NOT match"); // we will see this if it fails
 
     }
 
