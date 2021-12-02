7fdbcdde679a3aa36b53a68e8a7b573691f11351
OscarSah <oscar@cybertekschool.com>
2021-12-03 01:41:45 +0300

    First Test started

---
+++ src/test/java/com/cybertek/officeHours/oH03/priceTestOne.java
@@ -0,0 +1,51 @@
+package com.cybertek.officeHours.oH03;
+
+import com.cybertek.utils.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class priceTestOne {
+
+    WebDriver driver;
+    String url = "http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx";
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+        driver.get(url);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    /*
+ - Go to:  http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+- Login with username: Tester, password: test
+- Click  Order button
+- Verify under Product Information, selected option is “MyMoney”
+-Then select FamilyAlbum, make quantity 2, and click Calculate,
+- Then verify Total is equal to Quantity*PricePerUnit
+     */
+
+    @Test
+    public void testOne(){
+        // Login with username: Tester, password: test
+        driver.findElement(By.cssSelector("#ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.cssSelector("#ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
+
+
+
+
+
+    }
+
+}
