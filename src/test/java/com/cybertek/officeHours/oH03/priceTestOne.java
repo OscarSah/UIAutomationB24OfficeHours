71cac3d20ed17a7911a06def5412c50ab65b66d4
OscarSah <oscar@cybertekschool.com>
2021-12-03 02:15:07 +0300

    First Task is finished

--- src/test/java/com/cybertek/officeHours/oH03/priceTestOne.java
+++ src/test/java/com/cybertek/officeHours/oH03/priceTestOne.java
@@ -4,6 +4,7 @@ import com.cybertek.utils.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -39,7 +40,7 @@ public class priceTestOne {
      */
 
     @Test
-    public void testOne(){
+    public void testOne() throws InterruptedException {
         // Login with username: Tester, password: test
         driver.findElement(By.cssSelector("#ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.cssSelector("#ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
@@ -49,11 +50,33 @@ public class priceTestOne {
 
         // Verify under Product Information, selected option is “MyMoney”
         Select dropDown = new Select(driver.findElement(By.cssSelector("#ctl00_MainContent_fmwOrder_ddlProduct")));
-        String expectedText = "MyMone";
+        String expectedText = "MyMoney";
         String actualText = dropDown.getFirstSelectedOption().getText();
 
         Assert.assertEquals(actualText,expectedText,"Text do NOT match"); // we will see this if it fails
 
+        // Then select FamilyAlbum, make quantity 2, and click Calculate
+        dropDown.selectByValue("FamilyAlbum");
+        WebElement quantityBox = driver.findElement(By.cssSelector("#ctl00_MainContent_fmwOrder_txtQuantity"));
+        // int quantity = 2;
+       //  quantityBox.clear();
+        quantityBox.sendKeys(Keys.DELETE+"2");
+        Thread.sleep(2000);
+
+        WebElement pricePerUnitElement = driver.findElement(By.id("ctl00_MainContent_fmwOrder_txtUnitPrice"));
+        int pricePerUnit = Integer.parseInt(pricePerUnitElement.getAttribute("value"));
+        System.out.println("pricePerUnit = " + pricePerUnit);
+
+        // click calculate button
+        driver.findElement(By.xpath("//input[@value='Calculate']")).click();
+
+        int expectedTotalPrice = 2*pricePerUnit;
+        int actualTotalPrice = Integer.parseInt(driver.findElement(By.cssSelector("#ctl00_MainContent_fmwOrder_txtTotal")).getAttribute("value"));
+
+        System.out.println("expectedTotalPrice = " + expectedTotalPrice);
+        System.out.println("actualTotalPrice = " + actualTotalPrice);
+
+        Assert.assertEquals(actualTotalPrice,expectedTotalPrice,"The Price DO NOT match");
     }
 
 }
