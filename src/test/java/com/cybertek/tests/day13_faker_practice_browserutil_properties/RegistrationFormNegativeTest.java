36aee6da6fc245360abb3169eb9191656605767a
Murodil <murodil@cybertekschool.com>
2021-12-02 22:05:22 -0500

    DAY 12 - negative test

--- src/test/java/com/cybertek/tests/day13_faker_practice_browserutil_properties/RegistrationFormNegativeTest.java
+++ src/test/java/com/cybertek/tests/day13_faker_practice_browserutil_properties/RegistrationFormNegativeTest.java
@@ -4,6 +4,7 @@ import com.cybertek.utils.BrowserUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 public class RegistrationFormNegativeTest extends RegistrationFormTestBase {
     @Test
@@ -15,6 +16,18 @@ public class RegistrationFormNegativeTest extends RegistrationFormTestBase {
 
         //wait for 1 second
         BrowserUtils.sleep(2);
+
+        WebElement errorMsg = driver.findElement(By.xpath("//small[.='first name is required']"));
+        assertTrue(errorMsg.isDisplayed(), "Error message is not displayed");
+
+        //type 12345 and verify "first name can only consist of alphabetical letters" is displayed
+        WebElement firstName = driver.findElement(By.name("firstname"));
+        firstName.sendKeys("12345");
+
+        BrowserUtils.sleep(2);
+
+        WebElement noNumbersMsg = driver.findElement(By.xpath("//small[.='first name can only consist of alphabetical letters']"));
+        assertTrue(noNumbersMsg.isDisplayed(), "Error message is not displayed");
 
     }
 }
