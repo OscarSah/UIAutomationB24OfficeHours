205037c69b22c04c518974dae535d4557c8acfb7
Murodil <murodil@cybertekschool.com>
2021-12-04 10:54:51 -0500

    DAY 13 - smartbear task

---
+++ src/test/java/com/cybertek/tests/day14_utils_webtables_properties/SmartBearProductTest.java
@@ -0,0 +1,5 @@
+package com.cybertek.tests.day14_utils_webtables_properties;
+
+public class SmartBearProductTest {
+
+}
---
+++ src/test/java/com/cybertek/tests/day14_utils_webtables_properties/SmartBearTestBase.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day14_utils_webtables_properties;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class SmartBearTestBase {
+    @BeforeMethod
+    public void setUp() {
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+    }
+}
