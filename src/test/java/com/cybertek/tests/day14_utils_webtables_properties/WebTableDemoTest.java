f4ccee50c495699096ed36f9a99a3bcdcc1274ce
Murodil <murodil@cybertekschool.com>
2021-12-04 15:01:49 -0500

    DAY 13 - webtables practice

--- src/test/java/com/cybertek/tests/day14_utils_webtables_properties/WebTableDemoTest.java
+++ src/test/java/com/cybertek/tests/day14_utils_webtables_properties/WebTableDemoTest.java
@@ -6,9 +6,11 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.cybertek.utils.WebDriverFactory.getDriver;
+import static org.testng.Assert.*;
 
 public class WebTableDemoTest {
     WebDriver driver;
@@ -27,6 +29,27 @@ public class WebTableDemoTest {
         //Print table data as a single String, not so common, but useful sometimes
         WebElement table1 = driver.findElement(By.xpath("//table[@id='table1']"));
         System.out.println("Table 1 text = " + table1.getText());
+
+        //verify tconway@earthlink.net is anywhere in the table
+        assertTrue(table1.getText().contains("tconway@earthlink.net"));
+
+        //print all column names in single line
+        WebElement allColNames = driver.findElement(By.xpath("//table[@id='table1']/thead/tr"));
+        System.out.println("Column names : " + allColNames.getText());
+
+        //print each column name in separate lines using a loop and findElements method
+        List<WebElement> colNamesList = driver.findElements(By.xpath("//table[@id='table1']/thead/tr/th"));
+        System.out.println("Columns count = " + colNamesList.size());
+
+        for(WebElement colName : colNamesList) {
+            System.out.println("colName.getText() = " + colName.getText());
+        }
+        
+        //print first col + first cell data
+        String xpathStr = "//table[@id='table1']/tbody/tr[1]/td[1]";
+        WebElement firstRowCell = driver.findElement(By.xpath(xpathStr));
+        System.out.println("firstRowCell.getText() = " + firstRowCell.getText());
+        
     }
 
 
