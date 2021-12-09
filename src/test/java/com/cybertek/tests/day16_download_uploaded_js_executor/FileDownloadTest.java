a36cb8680be0c1f4c417a0757fb9867d07cb1c53
Murodil <murodil@cybertekschool.com>
2021-12-08 21:40:55 -0500

    DAY 16 - file download test

--- src/test/java/com/cybertek/tests/day16_download_uploaded_js_executor/FileDownloadTest.java
+++ src/test/java/com/cybertek/tests/day16_download_uploaded_js_executor/FileDownloadTest.java
@@ -1,9 +1,11 @@
 package com.cybertek.tests.day16_download_uploaded_js_executor;
 
 import com.cybertek.tests.TestBase;
+import com.cybertek.utils.BrowserUtils;
 import com.cybertek.utils.ConfigurationReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -17,12 +19,20 @@ public class FileDownloadTest extends TestBase {
         WebElement someFileDownload = driver.findElement(By.linkText("some-file.txt"));
         someFileDownload.click();
 
+        BrowserUtils.sleep(2);
+
         //verify that file is downloaded: /Users/cybertekb24/       Downloads/some-file.txt
         String filePath = System.getProperty("user.home") + "/Downloads/some-file.txt";
         System.out.println("filePath = " + filePath);
 
+        //build file object
         File file = new File( filePath );
         System.out.println("File exist = " + file.exists());
+
+        Assert.assertTrue(file.exists() , "File download failed");
+
+        //delete after verification complete
+        file.delete();
 
     }
 }
