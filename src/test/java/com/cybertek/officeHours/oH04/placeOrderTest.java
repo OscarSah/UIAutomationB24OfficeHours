48a27dc99c23a047d329f1ea02f23d96fc6b3197
OscarSah <oscar@cybertekschool.com>
2021-12-10 01:56:39 +0300

    Fourt Week Started

---
+++ src/test/java/com/cybertek/officeHours/oH04/placeOrderTest.java
@@ -0,0 +1,21 @@
+package com.cybertek.officeHours.oH04;
+
+public class placeOrderTest {
+    /*
+    DemoBlaze WebSite Order Confirmation
+ - Go to https://www.demoblaze.com/index.html#
+ - From Categories select Laptops, and from products select Sony Vaio i7
+ - click Add to Cart then handle pop up
+ - Navigate to Home
+ - From Categories select Phones, and from products select Iphone 6 32gb
+ - click Add to Cart then handle pop up
+ - From Categories select Monitors, and from products select Apple Monitor
+ - click Add to Cart then handle pop up
+ - Navigate to Cart
+ - Delete Iphone product from the Cart
+ - Then click on Place Order
+ - Fill Out the form with Faker Data
+ - Then Verify that expected (total of each individual product's price) is equal to actual (both the Cart price and Order amount) price
+ - Also capture order confirmation number, and verify customer's information
+     */
+}
---
+++ src/test/java/com/cybertek/utils/officeUtils.java
@@ -0,0 +1,32 @@
+package com.cybertek.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class officeUtils {
+
+// create a static method, will accept String, will return WebElement, getLink(String str)
+
+    public static WebElement getLink(String str){
+        return Driver.getDriver().findElement(By.partialLinkText(str));
+    }
+
+// addProduct(String category, String product), return an integer value which is product price
+// also add the product to my Cart
+
+    public static int addProduct(String category,String product){
+
+        getLink(category).click();
+        getLink(product).click();
+
+        // getting the product's price
+        WebElement priceContainer = Driver.getDriver().findElement(By.xpath("//h3"));
+        int productsPrice=Integer.parseInt(priceContainer.getText().substring(1,4));
+
+
+
+    }
+
+
+
+}
