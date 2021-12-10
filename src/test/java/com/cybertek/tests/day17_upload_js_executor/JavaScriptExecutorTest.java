542ea8abaa15ad4cb1d2bb332ed036cfa32befdf
Murodil <murodil@cybertekschool.com>
2021-12-09 20:59:52 -0500

    DAY 17 -js executor

--- src/test/java/com/cybertek/tests/day17_upload_js_executor/JavaScriptExecutorTest.java
+++ src/test/java/com/cybertek/tests/day17_upload_js_executor/JavaScriptExecutorTest.java
@@ -2,6 +2,7 @@ package com.cybertek.tests.day17_upload_js_executor;
 
 import com.cybertek.tests.TestBase;
 import com.cybertek.utils.BrowserUtils;
+import com.cybertek.utils.ConfigurationReader;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
@@ -23,6 +24,15 @@ public class JavaScriptExecutorTest extends TestBase {
 
     @Test
     public void scrollTest() {
+        driver.get(ConfigurationReader.getProperty("scroll.url"));
+        BrowserUtils.sleep(2);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 5000)");
+
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0, 1000)");
+        }
 
     }
 
