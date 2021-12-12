de7be2a8e9a8099b3b74749f611d5a8c95f689bd
Murodil <murodil@cybertekschool.com>
2021-12-12 11:36:46 -0500

    DAY 18 - dragAndDrop

---
+++ src/test/java/com/cybertek/tests/day19_page_object_model/DragAndDropTest.java
@@ -0,0 +1,26 @@
+package com.cybertek.tests.day19_page_object_model;
+
+import com.cybertek.tests.TestBase;
+import com.cybertek.utils.ConfigurationReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class DragAndDropTest extends TestBase {
+    @Test
+    public void guru99DragAndDropTest() {
+        driver.get(ConfigurationReader.getProperty("draganddrop.url"));
+
+        WebElement bank = driver.findElement(By.linkText("BANK"));
+        WebElement accountType = driver.findElement(By.id("bank"));
+
+        actions.dragAndDrop(bank, accountType).perform();
+
+        WebElement fiveK = driver.findElement(By.linkText("5000"));
+        WebElement bankAmount = driver.findElement(By.id("amt7"));
+
+        //actions.dragAndDropBy(fiveK, 793, 826).perform();
+        actions.dragAndDrop(fiveK , bankAmount).perform();
+
+    }
+}
